884a58a9422d52ee185f32a965faeb8ecb2e48c9
manuelnelson <elnels@gmail.com>
2018-07-11 14:54:23 -0400

    updated packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,16 @@
+/* nuxt.config.js */
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/<repository-name>/'
+  }
+} : {}
+
 module.exports = {
   /*
   ** Headers of the page
   */
+  ...routerBase,
   head: {
     title: 'Emmanuel Nelson',
     meta: [
