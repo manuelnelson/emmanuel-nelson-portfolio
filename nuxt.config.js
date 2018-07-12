421674447aac2e1d7f1629dd08afe102cab31c78
manuelnelson <elnels@gmail.com>
2018-07-12 10:40:17 -0400

    update favicon path

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,9 @@
 /* nuxt.config.js */
 // only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const base = process.env.DEPLOY_ENV === 'GH_PAGES' ? '/emmanuel-nelson-porfolio/' : '';
 const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
-    base: '/emmanuel-nelson-porfolio/'
+    base: base
   }
 } : {}
 
@@ -19,7 +20,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Portfolio' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: `${base}/favicon.ico` },
       { rel: 'stylesheet', type: 'text/css', href: '//fonts.googleapis.com/css?family=Noto+Sans:300,400,700|Rasa:300,400,700&subset=latin,latin-ext' }
     ]
   },
